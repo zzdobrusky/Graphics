a3d0cd2d10c0cafb6c55e9a65bad8edbfbad248e
zzdobrusky <zzdobrusky@gmail.com>
2014-11-21 14:56:21 -0700

    fixed missing glUseProgram call

--- app/src/main/java/edu/utah/cs4962/graphics/GraphicsActivity.java
+++ app/src/main/java/edu/utah/cs4962/graphics/GraphicsActivity.java
@@ -33,7 +33,7 @@ public class GraphicsActivity extends Activity implements GLSurfaceView.Renderer
     }
 
     @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config)
+    public void onSurfaceCreated(GL10 gl, EGLConfig eglConfig)
     {
         String vertexShaderSource = "" +
                 "attribute vec4 position; \n" +
@@ -67,37 +67,41 @@ public class GraphicsActivity extends Activity implements GLSurfaceView.Renderer
         _program = GLES20.glCreateProgram();
         GLES20.glAttachShader(_program, vertexShader);
         GLES20.glAttachShader(_program, fragmentShader);
+        // Bind variables
         GLES20.glBindAttribLocation(_program, POSITION_ATTRIBUTE_ID, "position");
         GLES20.glLinkProgram(_program);
+        GLES20.glUseProgram(_program);
         String programLinkLog = GLES20.glGetProgramInfoLog(_program);
         Log.i("Program Link", programLinkLog);
         GLES20.glClearColor(0.8f, 0.8f, 0.8f, 0.8f);
-        GLES20.glEnableVertexAttribArray(0);
+
     }
 
     @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height)
+    public void onSurfaceChanged(GL10 gl10, int width, int height)
     {
         GLES20.glViewport(0, 0, width, height);
     }
 
     @Override
-    public void onDrawFrame(GL10 gl)
+    public void onDrawFrame(GL10 gl10)
     {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
         float[] trianglePoints =
                 {
-                      -0.5f, -0.5f, 0.5f, 1.0f,
-                       0.5f, -0.5f, 0.5f, 1.0f,
-                       0.0f,  0.5f, 0.5f, 1.0f,
+                      -0.5f, -0.5f, 0.0f, 1.0f,
+                       0.5f, -0.5f, 0.0f, 1.0f,
+                       0.0f,  0.5f, 0.0f, 1.0f,
                 };
+
         ByteBuffer trianglePointsByteBuffer = ByteBuffer.allocateDirect(trianglePoints.length * 4);
         trianglePointsByteBuffer.order(ByteOrder.nativeOrder());
         FloatBuffer trianglePointsBuffer = trianglePointsByteBuffer.asFloatBuffer();
         trianglePointsBuffer.put(trianglePoints);
         trianglePointsBuffer.rewind();
 
+        GLES20.glEnableVertexAttribArray(0);
         GLES20.glVertexAttribPointer(POSITION_ATTRIBUTE_ID, 4, GLES20.GL_FLOAT, false, 4 * 4, trianglePointsBuffer);
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
     }
