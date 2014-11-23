256fcd7352d20fa841a9bf3539b787ad591a44f9
zzdobrusky <zzdobrusky@gmail.com>
2014-11-22 18:42:41 -0700

    lecture 22 - take 3

--- app/src/main/java/edu/utah/cs4962/graphics/GraphicsActivity.java
+++ app/src/main/java/edu/utah/cs4962/graphics/GraphicsActivity.java
@@ -1,8 +1,11 @@
 package edu.utah.cs4962.graphics;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.GLUtils;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -18,7 +21,7 @@ public class GraphicsActivity extends Activity implements GLSurfaceView.Renderer
 {
     int _program = -1;
     static final int POSITION_ATTRIBUTE_ID = 0;
-    static final int COLOR_ATTRIBUTE_ID = 1;
+    static final int TEXTURE_COORDINATE_ATTRIBUTE_ID = 1;
 
     float _translateX;
     float _translateY;
@@ -33,7 +36,6 @@ public class GraphicsActivity extends Activity implements GLSurfaceView.Renderer
         surfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
         surfaceView.setRenderer(this);
         setContentView(surfaceView);
-
     }
 
     @Override
@@ -41,23 +43,24 @@ public class GraphicsActivity extends Activity implements GLSurfaceView.Renderer
     {
         String vertexShaderSource = "" +
                 "attribute vec4 position; \n" +
-                "attribute vec4 color; \n" +
                 "uniform vec2 translate; \n" +
-                "varying vec4 colorVarying; \n" +
+                "attribute vec2 textureCoordinate; \n" +
+                "varying vec2 textureCoordinateVarying; \n" +
                 " \n" +
                 "void main() \n" +
                 "{ \n" +
                 "  gl_Position = vec4(position.x + translate.x, position.y + translate.y, position.z, position.w); \n" +
-                "  colorVarying = color;\n" +
+                "  textureCoordinateVarying = textureCoordinate;\n" +
                 "} \n" +
                 " \n";
 
         String fragmentShaderSource = "" +
-                "varying highp vec4 colorVarying; \n" +
+                "uniform sampler2D textureUnit; \n" +
+                "varying highp vec2 textureCoordinateVarying; \n" +
                 " \n" +
                 "void main() \n" +
                 "{ \n" +
-                "  gl_FragColor = colorVarying; \n" +
+                "  gl_FragColor = texture2D(textureUnit, textureCoordinateVarying); \n" +
                 "} \n" +
                 " \n";
 
@@ -78,17 +81,22 @@ public class GraphicsActivity extends Activity implements GLSurfaceView.Renderer
         GLES20.glAttachShader(_program, fragmentShader);
         // Bind variables
         GLES20.glBindAttribLocation(_program, POSITION_ATTRIBUTE_ID, "position");
+        GLES20.glBindAttribLocation(_program, TEXTURE_COORDINATE_ATTRIBUTE_ID, "textureCoordinate");
+        // link it and use it
         GLES20.glLinkProgram(_program);
         GLES20.glUseProgram(_program);
+        // trace linker output
         String programLinkLog = GLES20.glGetProgramInfoLog(_program);
         Log.i("Program Link", programLinkLog);
+        // make background gray
         GLES20.glClearColor(0.8f, 0.8f, 0.8f, 0.8f);
 
+        // triangle vertices coordinates
         float[] trianglePoints =
                 {
                         -0.5f, -0.5f, 0.0f, 1.0f,
                         0.5f, -0.5f, 0.0f, 1.0f,
-                        0.0f,  0.5f, 0.0f, 1.0f,
+                        0.0f,  0.5f, 0.0f, 1.0f
                 };
 
         ByteBuffer trianglePointsByteBuffer = ByteBuffer.allocateDirect(trianglePoints.length * 4);
@@ -97,24 +105,35 @@ public class GraphicsActivity extends Activity implements GLSurfaceView.Renderer
         trianglePointsBuffer.put(trianglePoints);
         trianglePointsBuffer.rewind();
 
-        GLES20.glEnableVertexAttribArray(POSITION_ATTRIBUTE_ID);
-        GLES20.glVertexAttribPointer(POSITION_ATTRIBUTE_ID, 4, GLES20.GL_FLOAT, false, 4 * 4, trianglePointsBuffer);
-
-        float[] triangleColors =
+        // triangle texture coordinates
+        float[] triangleTextureCoordinates =
                 {
-                        1.0f, 0.0f, 0.0f, 1.0f,
-                        0.0f, 1.0f, 0.0f, 1.0f,
-                        0.0f,  0.0f, 1.0f, 1.0f,
+                        0.0f, 1.0f,
+                        1.0f, 1.0f,
+                        0.5f, 0.0f
                 };
 
-        ByteBuffer triangleColorsByteBuffer = ByteBuffer.allocateDirect(triangleColors.length * 4);
-        triangleColorsByteBuffer.order(ByteOrder.nativeOrder());
-        FloatBuffer triangleColorsBuffer = triangleColorsByteBuffer.asFloatBuffer();
-        triangleColorsBuffer.put(triangleColors);
-        triangleColorsBuffer.rewind();
+        ByteBuffer triangleTextureByteBuffer = ByteBuffer.allocateDirect(triangleTextureCoordinates.length * 4);
+        triangleTextureByteBuffer.order(ByteOrder.nativeOrder());
+        FloatBuffer triangleTextureBuffer = triangleTextureByteBuffer.asFloatBuffer();
+        triangleTextureBuffer.put(triangleTextureCoordinates);
+        triangleTextureBuffer.rewind();
 
-        GLES20.glEnableVertexAttribArray(COLOR_ATTRIBUTE_ID);
-        GLES20.glVertexAttribPointer(COLOR_ATTRIBUTE_ID, 4, GLES20.GL_FLOAT, false, 4 * 4, triangleColorsBuffer);
+        GLES20.glEnableVertexAttribArray(POSITION_ATTRIBUTE_ID);
+        GLES20.glVertexAttribPointer(POSITION_ATTRIBUTE_ID, 4, GLES20.GL_FLOAT, false, 4 * 4, trianglePointsBuffer);
+        GLES20.glEnableVertexAttribArray(TEXTURE_COORDINATE_ATTRIBUTE_ID);
+        GLES20.glVertexAttribPointer(TEXTURE_COORDINATE_ATTRIBUTE_ID, 2, GLES20.GL_FLOAT, false, 2 * 4, triangleTextureBuffer);
+
+
+        // loading texture
+        Bitmap astronomyTexture = BitmapFactory.decodeResource(getResources(), R.drawable.astronomy);
+        int[] textureIds = new int[1];
+        GLES20.glGenTextures(1, textureIds, 0);
+        int textureId = textureIds[0];
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, astronomyTexture, 0);
     }
 
     @Override
@@ -126,10 +145,11 @@ public class GraphicsActivity extends Activity implements GLSurfaceView.Renderer
     @Override
     public void onDrawFrame(GL10 gl10)
     {
-        _translateX += 0.001f;
-        _translateY += 0.0005f;
+        _translateX += 0.0f;//01f;
+        _translateY += 0.0f;//005f;
 
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
         int translateLocation = GLES20.glGetUniformLocation(_program, "translate");
         GLES20.glUniform2f(translateLocation, _translateX, _translateY);
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
